dc2fc8c78b1135567e371301430cbecf3097afd7
aoba <ipbdipbd00@gmail.com>
2019-04-20 20:40:28 +0900

    Execute annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint(8)        not null, primary key
+#  email             :string(255)      not null
+#  name              :string(255)      not null
+#  password_digest   :string(255)      not null
+#  remember_digest   :string(255)
+#  admin             :boolean          default(FALSE)
+#  activation_digest :string(255)
+#  activated         :boolean          default(FALSE)
+#  activated_at      :datetime
+#  reset_digest      :string(255)
+#  reset_sent_at     :datetime
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class User < ApplicationRecord
   # accessor
   attr_accessor :remember_token, :activation_token, :reset_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# == Route Map
+#
+#                  Prefix Verb   URI Pattern                             Controller#Action
+#                   hello GET    /hello(.:format)                        application#hello
+#                   login POST   /login(.:format)                        sessions#create
+#                  logout DELETE /logout(.:format)                       sessions#destroy
+#                me_users GET    /users/me(.:format)                     users#me
+#                   users POST   /users(.:format)                        users#create
+# edit_account_activation GET    /account_activations/:id/edit(.:format) account_activations#edit
+#         password_resets POST   /password_resets(.:format)              password_resets#create
+#          password_reset PATCH  /password_resets/:id(.:format)          password_resets#update
+#                         PUT    /password_resets/:id(.:format)          password_resets#update
+
 Rails.application.routes.draw do
 
   get '/hello', to: 'application#hello'
